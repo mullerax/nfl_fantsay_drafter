e270ca5357eb978c785c292f67646762b49b7716
Axel Müller <38631622+mullerax@users.noreply.github.com>
2021-04-18 11:04:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+"""Steps:
+- 1: Scrap player data from "https://www.pro-football-reference.com/ for specific season
+- 2: Calculate fantasy points depending on rules
+- 3: Find best draft strategy:
+    a) Based on 'rules'
+    b) Some kind of AI (This is for learning)"""
