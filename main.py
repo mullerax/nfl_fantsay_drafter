a6607011b99483876e0065db3b06a8da36b80960
Axel Müller <38631622+mullerax@users.noreply.github.com>
2021-04-18 11:31:09 +0200

    Implement test routine to check if website is available

--- main.py
+++ main.py
@@ -4,3 +4,18 @@
 - 3: Find best draft strategy:
     a) Based on 'rules'
     b) Some kind of AI (This is for learning)"""
+
+import urllib.request
+import urllib.error
+import sys
+"""Get data from website"""
+url = "https://www.pro-football-reference.com/years/"
+year = 2020
+
+"""Check if website is available"""
+try:
+    url_answer_code = urllib.request.urlopen(url+str(year)).getcode()
+except urllib.error.URLError:
+    print("Page not found")
+    sys.exit(1)
+
