b894efc23befe7ec22f81c2aedbd0aea20d8b881
Axel Müller <38631622+mullerax@users.noreply.github.com>
2021-06-07 22:38:19 +0200

    Add rushing and receiving category

--- scrap_player_data.py
+++ scrap_player_data.py
@@ -14,7 +14,8 @@ def get_player_data(url):
         - Kicking/Punting
     => Later: Combine lists to get sort data for each player"""
     lists = ["passing", "rushing", "receiving"]
-    category_list_url = url + lists[0] + ".htm"
+    category_list_url = url + lists[2] + ".htm"
+    cat = 'receiving'
     data = scrap_category(category_list_url, cat)
     return data
 
@@ -29,21 +30,37 @@ def scrap_category(cat_url, cat):
     for row in table.find_all('tr'):
         # print(row)
         columns = row.find_all('td')
+        player_dict = 0
         player_name = 0
         player_team = 0
         player_position = 0
         player_url = 0
+
         # passing
-        pass_attempts = 0
+        pass_att = 0
         pass_cmp = 0
         pass_yds = 0
         pass_td = 0
         pass_int = 0
         sacks = 0
         sack_loss = 0
+
+        # rushing
+        rush_att = 0
+        rush_yds = 0
+        rush_td = 0
+        rush_fumbles = 0
+
+        # receiving
+        targets = 0
+        receptions = 0
+        receiving_yds = 0
+        receiving_td = 0
+        rec_fumbles = 0
+
         for element in columns:
-            # print(element.get("data-stat"))
-            # print(element.text)
+            print(element.get("data-stat"))
+            print(element.text)
 
             if element.get("data-stat") == "player":
 
@@ -65,7 +82,7 @@ def scrap_category(cat_url, cat):
 
             if cat == 'passing':
                 if element.get("data-stat") == "pass_att":
-                    pass_attempts = element.getText()
+                    pass_att = element.getText()
 
                 if element.get("data-stat") == "pass_cmp":
                     pass_cmp = element.getText()
@@ -85,13 +102,48 @@ def scrap_category(cat_url, cat):
                 if element.get("data-stat") == "pass_sacked_yds":
                     sack_loss = element.getText()
 
-            player_dict = {"Name": player_name, "URL": player_url, "Position": player_position, "Team": player_team,
-                           "Attempts": pass_attempts, "Completions": pass_cmp, "Pass yards": pass_yds, "Pass td": pass_td,
-                           "Pass int":pass_int, "Sacks": sacks, "Sack loss yds": sack_loss}
+                player_dict = {"Name": player_name, "URL": player_url, "Position": player_position, "Team": player_team,
+                               "Pass att": pass_att, "Pass comp": pass_cmp, "Pass yards": pass_yds,
+                               "Pass td": pass_td, "Pass int": pass_int, "Sacks": sacks, "Sack loss yds": sack_loss}
+            if cat == 'rushing':
+                if element.get("data-stat") == "rush_att":
+                    rush_att = element.getText()
+
+                if element.get("data-stat") == "rush_yds":
+                    rush_yds = element.getText()
+
+                if element.get("data-stat") == "rush_td":
+                    rush_td = element.getText()
+
+                if element.get("data-stat") == "fumbles":
+                    rush_fumbles = element.getText()
+                player_dict = {"Name": player_name, "URL": player_url, "Position": player_position, "Team": player_team,
+                               "Rush att": rush_att, "Rush yards": rush_yds, "Rush td": rush_td, "Fumbles": rush_fumbles
+                               }
+
+            if cat == 'receiving':
+                if element.get("data-stat") == "targets":
+                    targets = element.getText()
+
+                if element.get("data-stat") == "rec":
+                    receptions = element.getText()
+
+                if element.get("data-stat") == "rec_yds":
+                    receiving_yds = element.getText()
+
+                if element.get("data-stat") == "rec_td":
+                    receiving_td = element.getText()
+
+                if element.get("data-stat") == "fumbles":
+                    rec_fumbles = element.getText()
+                player_dict = {"Name": player_name, "URL": player_url, "Position": player_position, "Team": player_team,
+                               "Pass targets": targets, "Pass receptions": receptions, "Receiving yards": receiving_yds,
+                               "Receiving td": receiving_td, "Fumbles": rec_fumbles}
+            else:
+                break
         if player_name != 0:
             player_list.append(player_dict)
     frame = pd.DataFrame(player_list)
     # with pd.option_context('display.max_rows', None, 'display.max_columns', None):
     #     print(frame)
     return frame
-
